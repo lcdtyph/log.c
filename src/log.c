5cb6923c7b8aec4dad8526f710d21be481bba41e
lcdtyph <lcdtyph@gmail.com>
2019-03-05 17:54:14 +0800

    add pid to log, refine log format

--- src/log.c
+++ src/log.c
@@ -25,6 +25,8 @@
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "log.c/log.h"
 
@@ -86,6 +88,17 @@ void log_set_quiet(int enable) {
   L.quiet = enable ? 1 : 0;
 }
 
+static int get_time_string(char *time_buf, int len) {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  char tmp[20];
+  int time_size = strftime(tmp, sizeof tmp, "%D %T", gmtime(&ts.tv_sec));
+  return snprintf(time_buf, len, "[%s.%06ldUTC]", tmp, ts.tv_nsec / 1000);
+}
+
+static int _get_pid() {
+    return getpid();
+}
 
 void log_log(int level, const char *file, int line, const char *fmt, ...) {
   if (level < L.level) {
@@ -96,20 +109,18 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
   lock();
 
   /* Get current time */
-  time_t t = time(NULL);
-  struct tm *lt = localtime(&t);
+  char time_buf[64];
+  get_time_string(time_buf, sizeof time_buf);
 
   /* Log to stderr */
   if (!L.quiet) {
     va_list args;
-    char buf[16];
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_USE_COLOR
     fprintf(
-      stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
-      buf, level_colors[level], level_names[level], file, line);
+      stderr, "%s%-5s\x1b[0m %s %8d \x1b[90m%s:%d:\x1b[0m ",
+      level_colors[level], level_names[level], time_buf, _get_pid(), file, line);
 #else
-    fprintf(stderr, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+    fprintf(stderr, "%-5s %s %8d %s:%d: ", level_names[level], time_buf, _get_pid(), file, line);
 #endif
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
@@ -121,9 +132,7 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
   /* Log to file */
   if (L.fp) {
     va_list args;
-    char buf[32];
-    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-    fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], file, line);
+    fprintf(L.fp, "%-5s %s %8d %s:%d: ", level_names[level], time_buf, _get_pid(), file, line);
     va_start(args, fmt);
     vfprintf(L.fp, fmt, args);
     va_end(args);
